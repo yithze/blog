cb76e123569e98d2c1e82897e712250d70d6e657
Nithze <aldiyusronzhar@gmail.com>
2024-11-28 08:48:33 +0700

    Initial commit

---
+++ app/projects/page.tsx
@@ -0,0 +1,15 @@
+import { BlogPosts } from 'app/components/posts'
+
+export const metadata = {
+  title: 'Projects',
+  description: 'My projects',
+}
+
+export default function Page() {
+  return (
+    <section>
+      <h1 className="font-semibold text-2xl mb-8 tracking-tighter">My Projects</h1>
+      <BlogPosts />
+    </section>
+  )
+}
